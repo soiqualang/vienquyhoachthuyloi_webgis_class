2e434f1c3a62871788e4867cf7f4ec2fded31009
soiqualang <soiqualang.chentreu@gmail.com>
2020-06-06 15:01:50 +0700

    t1

--- buoi5_php/php_sql.php
+++ buoi5_php/php_sql.php
@@ -4,7 +4,7 @@
 	define("PG_HOST","localhost");
 	define("PG_USER","postgres");
 	define("PG_PORT","5433");
-	define("PG_PASS","****8");
+	define("PG_PASS","****");
 	
 	$dbcon=pg_connect("dbname=".PG_DB." user=".PG_USER." password=".PG_PASS." host=".PG_HOST." port=".PG_PORT);
 	
