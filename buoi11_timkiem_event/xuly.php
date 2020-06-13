7e242a404119b41b6cbcf63146bd539c55db2bfa
soiqualang <soiqualang.chentreu@gmail.com>
2020-06-13 09:21:58 +0700

    t1

--- buoi11_timkiem_event/xuly.php
+++ buoi11_timkiem_event/xuly.php
@@ -8,22 +8,45 @@
 		$shthua=$_GET['shthua'];
 		
 		//Noi dung xu ly
-		//Khi co du shbando,shthua
-		$truyvan='SELECT * FROM "duynghia_quangnam" WHERE "shbando" = \''.$shbando.'\' AND "shthua" = \''.$shthua.'\' LIMIT 50';
+		if($shbando!='' and $shthua!=''){
+			//Khi co du shbando,shthua
+			$truyvan='SELECT * FROM "duynghia_quangnam" WHERE "shbando" = \''.$shbando.'\' AND "shthua" = \''.$shthua.'\' LIMIT 50';
+		}else if($shbando!='' and $shthua==''){
+			//Có số tờ, không có số thửa
+			$truyvan='SELECT * FROM "duynghia_quangnam" WHERE "shbando" = \''.$shbando.'\' LIMIT 50';
+		}else if($shbando!='' and $shthua==''){
+			//Không có số tờ, Có số thửa
+			$truyvan='SELECT * FROM "duynghia_quangnam" WHERE "shthua" = \''.$shthua.'\' LIMIT 50';
+		}else{
+			//Không có số tờ, không có số thửa
+			$truyvan='SELECT * FROM "duynghia_quangnam" LIMIT 50';
+		}
 		
-		echo $truyvan;
 		
-		/* $thucthi=pg_query($dbcon,$truyvan);
+		//echo $truyvan;
+		
+		$thucthi=pg_query($dbcon,$truyvan);
 		//check co ket qua hay khong
 		if(pg_num_rows($thucthi)>0){
 			//Lay du lieu ra
+			echo '<table border="1">';
+			echo '<tr>
+					<td><b>Tên</b></td>
+					<td><b>Địa chỉ</b></td>
+				</tr>';
 			while($kq=pg_fetch_assoc($thucthi)){
-				echo 'Tên '.$kq['tenchusdd'].', địa chỉ là '.$kq['diachi'];
-				echo '<br>';
+				/* echo 'Tên '.$kq['tenchusdd'].', địa chỉ là '.$kq['diachi'];
+				echo '<br>'; */
+				
+				echo '<tr>
+						<td>'.$kq['tenchusdd'].'</td>
+						<td>'.$kq['diachi'].'</td>
+					</tr>';
 			}
+			echo '</table>';
 		}else{
 			echo 'Không tìm thấy kết quả phù hợp.';
-		} */
+		}
 		
 		
 		
